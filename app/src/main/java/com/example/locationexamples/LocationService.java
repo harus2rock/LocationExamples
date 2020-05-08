7dfa8effc527431b7e0a1e2f7301a2ecdba0b8b7
harus2rock <izumi@katolab.nitech.ac.jp>
2020-05-09 04:21:56 +0900

    Complete Vol.1 of the reference

---
+++ app/src/main/java/com/example/locationexamples/LocationService.java
@@ -0,0 +1,164 @@
+package com.example.locationexamples;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import static android.content.ContentValues.TAG;
+import static android.location.GpsStatus.*;
+
+public class LocationService extends Service implements LocationListener, Listener {
+    public static final String LOG_TAG = LocationService.class.getSimpleName();
+
+    private final LocationServiceBinder binder = new LocationServiceBinder();
+    boolean isLocationManagerUpdatingLocation;
+
+    public LocationService() {
+    }
+
+    @Override
+    public void onCreate() {
+        isLocationManagerUpdatingLocation = false;
+    }
+
+    @Override
+    public int onStartCommand(Intent i, int flags, int startId) {
+        super.onStartCommand(i, flags, startId);
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+
+    @Override
+    public void onRebind(Intent intent) {
+        Log.d(LOG_TAG, "onRebind ");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(LOG_TAG, "onUnbind ");
+        return true;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy ");
+    }
+
+    //This is where we detect the app is being killed, thus stop service.
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        Log.d(LOG_TAG, "onTaskRemoved ");
+
+        if(this.isLocationManagerUpdatingLocation){
+            this.stopUpdatingLocation();
+            isLocationManagerUpdatingLocation = false;
+        }
+
+        stopSelf();
+    }
+
+    /**
+     * Binder class
+     * @author Takamitsu Mizutori
+     *
+     */
+    class LocationServiceBinder extends Binder {
+        LocationService getService() {
+            return LocationService.this;
+        }
+    }
+
+    @Override
+    public void onGpsStatusChanged(int event) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location newLocation) {
+        Log.d(TAG, "(" + newLocation.getLatitude() + "," + newLocation.getLongitude() + ")");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
+            if (status == LocationProvider.OUT_OF_SERVICE) {
+                notifyLocationProviderStatusUpdated(false);
+            } else {
+                notifyLocationProviderStatusUpdated(true);
+            }
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        if (provider.equals(LocationManager.GPS_PROVIDER)){
+            notifyLocationProviderStatusUpdated(false);
+        }
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
+            notifyLocationProviderStatusUpdated(true);
+        }
+    }
+
+    private void notifyLocationProviderStatusUpdated(boolean isLocationProviderAvailable) {
+        // Broadcast location provider status change here
+    }
+
+    public void startUpdatingLocation() {
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        try {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setPowerRequirement(Criteria.POWER_HIGH);
+            criteria.setAltitudeRequired(false);
+            criteria.setSpeedRequired(false); // calculate speed: false
+            criteria.setCostAllowed(true);
+            criteria.setBearingRequired(false); // direction information
+
+            criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+            criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+
+            int gpsFreqInMillis = 1000;
+            int gpsFreqInDistance = 1; // in meters
+
+            locationManager.addGpsStatusListener(this);
+            locationManager.requestLocationUpdates(gpsFreqInMillis, gpsFreqInDistance, criteria, this, null);
+
+        } catch (IllegalArgumentException e){
+            Log.e(LOG_TAG, e.getLocalizedMessage());
+        } catch (SecurityException e) {
+            Log.e(LOG_TAG, e.getLocalizedMessage());
+        } catch (RuntimeException e) {
+            Log.e(LOG_TAG, e.getLocalizedMessage());
+        }
+    }
+
+    public  void stopUpdatingLocation(){
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationManager.removeUpdates(this);
+    }
+}
--- app/src/main/java/com/example/locationexamples/MainActivity.java
+++ app/src/main/java/com/example/locationexamples/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.locationexamples;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public LocationService locationService;
+    private static final int REQUEST_CODE_PERMISSION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(this, "Permission is off.", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_PERMISSION);
+            }
+            return;
+        }
+        final Intent serviceStart = new Intent(this.getApplication(), LocationService.class);
+        this.getApplication().startService(serviceStart);
+        this.getApplication().bindService(serviceStart, serviceConnection, Context.BIND_AUTO_CREATE);
     }
+
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            String name = className.getClassName();
+
+            if (name.endsWith("LocationService")) {
+                locationService = ((LocationService.LocationServiceBinder) service).getService();
+
+                locationService.startUpdatingLocation();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName className) {
+            if (className.getClassName().equals("LocationService")) {
+                locationService = null;
+            }
+
+        }
+    };
 }
