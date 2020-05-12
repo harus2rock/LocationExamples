56458c7ea18f8bd7ca5400d9141c4d9017c962df
harus2rock <izumi@katolab.nitech.ac.jp>
2020-05-13 02:46:38 +0900

    Use GoogleMapAPI and Fragment

--- app/src/main/java/com/example/locationexamples/MainActivity.java
+++ app/src/main/java/com/example/locationexamples/MainActivity.java
@@ -3,27 +3,46 @@ package com.example.locationexamples;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.app.FragmentTransaction;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final String TAG = "MainActivity";
 
     public LocationService locationService;
     private static final int REQUEST_CODE_PERMISSION = 2;
+
+    private GoogleMap mMap;
+    private LatLng latlng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Permission check
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
@@ -36,11 +55,42 @@ public class MainActivity extends AppCompatActivity {
             }
             return;
         }
+
+        // LocationService
         final Intent serviceStart = new Intent(this.getApplication(), LocationService.class);
         this.getApplication().startService(serviceStart);
         this.getApplication().bindService(serviceStart, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        // Map
+        SupportMapFragment mapFragment =
+               (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Test to visible GoogleMap
+//        double latitude = 34.97;
+//        double longitude = 137.152;
+//
+//        latlng = new LatLng(latitude, longitude);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 14));
+
+        // my settings
+        mMap.getUiSettings().setAllGesturesEnabled(false);
+        mMap.setMyLocationEnabled(false);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        // Auto zoom settings
+        mMap.setOnCameraMoveStartedListener(new GoogleMap.OnCameraMoveStartedListener() {
+            @Override
+            public void onCameraMoveStarted(int reason) {
+            }
+        });
+    }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
@@ -62,4 +112,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
 }
